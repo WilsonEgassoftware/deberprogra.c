813e215742b233955e1db20af0c5ca852b5838e3
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2023-10-20 22:29:50 -0500

    Update ejercicio14.c

--- ejercicio14.c
+++ ejercicio14.c
@@ -5,13 +5,13 @@ int main (){ //Inicializamos las varibles que usaremos , como almancenar los 10 
     int promedio=0;
     int contador; // contador de las veces que repita
     int numeros [10];//Almacenar lo numeros
-    int n=10;
+    int n=10;// cantidad de numeros que vamos a leer
      //pedimos el ingreso de  los datos 
     printf("Ingrese 10 numero enteros:");
     //creamos un ciclo hasta 10 para que sea almacenen los datos 
    for (int i = 0;  i < n; i++ ){
      scanf("%d",&numero);
-     suma = numeros[i];
+     suma = numeros[i];//realizar el procedmiento para poder encontrar el promedio
 
    }
    // calcular al promedio
