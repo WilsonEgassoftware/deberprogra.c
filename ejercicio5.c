1b66e40bb8d91e9a76c2ed9b5e9aa297a567f1a3
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2023-10-21 18:49:20 -0500

    Update ejercicio5.c

--- ejercicio5.c
+++ ejercicio5.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int esPrimo(int num) {
+int esPrimo(int num) {// Función para verificar si un número es primo
     if (num <= 1) {
         return 0; // 0 y 1 no son primos
     }
@@ -11,13 +11,13 @@ int esPrimo(int num) {
     return 1; // Es primo
 }
 int main(){
-    int numero;
+    int numero;//declaramos los datos de verifiacacion de los datos
     int Primo;
     int i;
     int contador = 0;
    int primos[10];
     //buscar numeros primos entre 100 y 300
-    for (int i = 100; i <= 300 && contador < 10; i++) {
+    for (int i = 100; i <= 300 && contador < 10; i++) {//obtenemos y buscamos hasta cuando se puede obtener los datos
         if (esPrimo (i)){
             primos[contador] = i ;
             contador++;
