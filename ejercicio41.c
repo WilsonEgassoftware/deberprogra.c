bd376cbe3fabe00e9d0919295a3518c838bed825
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2023-10-22 17:08:07 -0500

    Update ejercicio41.c

--- ejercicio41.c
+++ ejercicio41.c
@@ -17,7 +17,7 @@ int main() {
             contador++; // Número igual a 0 se considera como comenzando con 3.
         } else {
             while (primerDigito < -9 || (primerDigito > 0 && primerDigito > 9)) {// la condicional de los mayores que deben ser menores a 10 positivos y negativos
-                primerDigito / 10;
+                primerDigito / 10;//division de los datos sobre le total de 10 valores
             }
             if (primerDigito == 3) {//si el primer digitico cumple la condición se suman al contador
                 contador++;//suma del contador
