df7a3bb5cf8f019ccd0d04dffcbf2243997841f2
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2023-10-22 16:20:31 -0500

    Update ejercicio32.c

--- ejercicio32.c
+++ ejercicio32.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main (){
-    int numero[10];
-    int referencia;
+    int numero[10];// cantidad de espacio que usaremos
+    int referencia;//numero que nos servira verificar sus datos solo 10 datos
     int contador=0;
     int ultimoDigito= referencia/10;
 
@@ -18,12 +18,12 @@ int main (){
     //A través del vector y contar cuántos números terminan en el mismo dígito.
     for(int i=0; i<10;i++){
         int ultimoDigito = numero[i]/10;
-        if(ultimoDigito==ultimoDigito){
+        if(ultimoDigito==ultimoDigito){//obtenemos el resultado de los datos con una division entre 10 del total
 
         } 
     }
     //obtener el resultado
-    printf("El resultado de la misma cantidad de digitos es %d:%d\n", referencia, contador);
+    printf("El resultado de la misma cantidad de digitos es %d:%d\n", referencia, contador);//aumentado la referencia y el contador
 
     return 0;
 }
